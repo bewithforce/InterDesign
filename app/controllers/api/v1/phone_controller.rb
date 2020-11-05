8bf39e394b4ce8830f44425f985e224a36abc7fb
Dzianis <somniummisellus@gmail.com>
2020-11-05 20:08:37 +0300

    sweet&sour

--- app/controllers/api/v1/phone_controller.rb
+++ app/controllers/api/v1/phone_controller.rb
@@ -4,10 +4,11 @@ require 'json'
 class Api::V1::PhoneController < ApplicationController
   def csv
     csv_text = File.read('public/table.csv')
+    print csv_text
     csv = CSV.parse(csv_text, :headers => true)
     settings_text = File.read('public/configs.json')
     settings = JSON.parse(settings_text)
-    csv
+    csv_text
 
   end
 end
