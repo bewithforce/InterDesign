aaaf3b44acd56656bde6ab235c33be3034131f96
Dzianis <somniummisellus@gmail.com>
2020-11-06 20:03:52 +0300

    darlin'

--- app/controllers/api/v1/phone_controller.rb
+++ app/controllers/api/v1/phone_controller.rb
@@ -4,20 +4,42 @@ require 'aws-sdk-s3'
 
 class Api::V1::PhoneController < ApplicationController
   def csv
-    Aws::S3::Client.new(
+    s3 = Aws::S3::Client.new(
         region: 'eu-central-1',
         access_key_id: 'AKIA6EUXVWM3WMVOJSON',
         secret_access_key: '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'
     )
-    resp = s3.get_object(bucket: 'inter-design', key: 'configs.json')
-    print resp
+    key = params[:key]
+    value = params[:value]
 
-    csv_text = open().read
+    settings = JSON.parse(s3.get_object(bucket: 'inter-design', key: 'configs.json').body.read)
+    settings = settings['settings']
+    settings = settings.select { |e| e['key'].to_s == key.to_s }[0]
 
-    csv = CSV.parse(csv_text, :headers => true)
-    settings_text = File.read('public/configs.json')
-    settings = JSON.parse(settings_text)
-    csv_text
+    primary = settings['primary']
+    access = settings['access']
+    selected_row = []
+
+    csv = CSV.parse(s3.get_object(bucket: 'inter-design', key: 'table.csv').body.read)
+
+
+    for i in 0..csv.length() - 1 do
+      if csv[i][primary].to_s == value
+        selected_row = csv[i]
+        break
+      end
+    end
+
+    to_return = {'return': []}
+    if selected_row.length() != 0
+      access.each do |n|
+        to_return[:return].push(selected_row[n])
+      end
+    end
+
+    puts to_return
+
+    render json: to_return
 
   end
 end
