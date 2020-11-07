b3250f129cf7ad0159f4804f4ce667519f1b2c7d
Dzianis <somniummisellus@gmail.com>
2020-11-07 16:11:39 +0300

    tick tock

--- app/controllers/api/v1/phone_controller.rb
+++ app/controllers/api/v1/phone_controller.rb
@@ -3,27 +3,40 @@ require 'json'
 require 'aws-sdk-s3'
 
 class Api::V1::PhoneController < ApplicationController
-  def csv
-    s3 = Aws::S3::Client.new(
+  skip_before_action :verify_authenticity_token
+
+  def initialize
+    super
+    @s3 = Aws::S3::Client.new(
         region: 'eu-central-1',
-        access_key_id: 'AKIA6EUXVWM3WMVOJSON',
-        secret_access_key: '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'
+        access_key_id: 'AKIA6EUXVWM36ARXUKXN',
+        secret_access_key: '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'
     )
+  end
+
+  def csv
     key = params[:key]
     value = params[:value]
 
-    settings = JSON.parse(s3.get_object(bucket: 'inter-design', key: 'configs.json').body.read)
-    settings = settings['settings']
-    settings = settings.select { |e| e['key'].to_s == key.to_s }[0]
+    settings = JSON.parse(@s3.get_object(bucket: 'inter-design', key: 'configs.json').body.read)
+
+    puts settings
+
+    settings = settings.select { |e| e['key'].to_s == key.to_s }
+
+    if settings.length == 0
+      render json: {}
+      return
+    end
+    settings = settings[0]
 
     primary = settings['primary']
     access = settings['access']
     selected_row = []
 
-    csv = CSV.parse(s3.get_object(bucket: 'inter-design', key: 'table.csv').body.read)
+    csv = CSV.parse(@s3.get_object(bucket: 'inter-design', key: 'table.csv').body.read)
 
-
-    for i in 0..csv.length() - 1 do
+    for i in 0..csv.length - 1 do
       if csv[i][primary].to_s == value
         selected_row = csv[i]
         break
@@ -31,15 +44,30 @@ class Api::V1::PhoneController < ApplicationController
     end
 
     to_return = {'data': []}
-    if selected_row.length() != 0
+    if selected_row.length != 0
       access.each do |n|
         to_return[:data].push(selected_row[n])
       end
     end
 
-    puts to_return
-
     render json: to_return
+  end
 
+  def save_settings
+    new_settings = params[:_json].as_json.to_json
+    @s3.delete_object(bucket: 'inter-design', key: 'configs.json')
+    @s3.put_object(bucket: 'inter-design', key: 'configs.json', body: new_settings)
+  end
+
+  def load_settings
+    settings = @s3.get_object(bucket: 'inter-design', key: 'configs.json').body.read
+    render json: settings
+  end
+
+  def save_file
+    new_file = params[:csv]
+    puts new_file
+    @s3.delete_object(bucket: 'inter-design', key: 'table.csv')
+    @s3.put_object(bucket: 'inter-design', key: 'table.csv', body: new_file)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
       get 'app/index'
 
       get 'phone/csv'
+      post 'phone/save_settings'
+      get 'phone/load_settings'
+      post 'phone/save_file'
     end
   end
   root 'main#index'
